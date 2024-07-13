a80a8d4ce1d1c5ed3b4ed96e36bf7bc3e8175605
EvertnRS <evertosilves@gmail>
2024-07-12 21:12:18 -0300

    start user validation

--- src/main/java/br/upe/persistence/Persistence.java
+++ src/main/java/br/upe/persistence/Persistence.java
@@ -1,6 +1,6 @@
-package br.upe.controller;
+package br.upe.persistence;
 
-public interface Controller {
+public interface  Persistence {
     public void create(Object... params);
     public void delete();
     public void update();
--- src/main/java/br/upe/ui/Interface.java
+++ src/main/java/br/upe/ui/Interface.java
@@ -2,37 +2,49 @@ package br.upe.ui;
 
 import java.util.Scanner;
 
+import br.upe.controller.Controller;
+import br.upe.controller.UserController;
+
+
 public class Interface
 {
     public static void main( String[] args )
     {
         System.out.println( "Bem-Vindo ao Even4" );
-        int option = -1;
-        Scanner sc = new Scanner( System.in );
-        String email = "";
-        do {
-            System.out.println( "[ 1 ] - Login" );
-            System.out.println( "[ 2 ] - Cadastrar" );
-            System.out.println( "[ 0 ] - Sair" );
-            option = sc.nextInt();
-            sc.nextLine();
-            switch (option) {
-                case 1:
-                    System.out.println( "Digite seu email:" );
-                    email = sc.nextLine();
+        int option;
+        try (Scanner sc = new Scanner( System.in )) {
+            do {
+                System.out.println( "[ 1 ] - Login" );
+                System.out.println( "[ 2 ] - Cadastrar" );
+                System.out.println( "[ 0 ] - Sair" );
+                option = sc.nextInt();
+                sc.nextLine();
+                switch (option) {
+                    case 1 -> login();
+                    case 2 -> signin();
+                }
+                
+            } while(option != 0);
+        }
+    }
 
-                    //validar email
-                    break;
-                case 2:
-                    System.out.println( "Cadastre seu email:" );
-                    email = sc.nextLine();
-                    System.out.println( "digite seu nome:" );
-                    String nome = sc.nextLine();
+    public static void login() {
+        try (Scanner sc = new Scanner( System.in )) {
+            System.out.println( "Digite seu email:" );
+            String email = sc.nextLine();
+            //validar email
+        }
+    }
 
-                    //cadastrar email
-            }
-
-        } while(option != 0);
-        sc.close();
+    public static void signin() {
+        try (Scanner sc = new Scanner( System.in )) {
+            System.out.println( "Cadastre seu email:" );
+            String email = sc.nextLine();
+            System.out.println( "digite seu nome:" );
+            String name = sc.nextLine();
+            
+            Controller userController = new UserController();
+            userController.create(email, name);
+        }
     }
 }
