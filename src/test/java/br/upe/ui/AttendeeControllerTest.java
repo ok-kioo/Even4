9d8027c814313dccf3af3d65331a0f86f34c231f
kio <kaio.viana@upe.br>
2024-10-04 11:19:20 -0300

    Merge remote-tracking branch 'origin/master'

--- src/test/java/br/upe/ui/AttendeeControllerTest.java
+++ src/test/java/br/upe/ui/AttendeeControllerTest.java
@@ -32,12 +32,7 @@ public class AttendeeControllerTest {
     @Test
     public void testCreateAttendee() throws FileNotFoundException {
         userController.create("newuser@example.com", "09876543211");
-
-        if (userController.loginValidate("newuser@example.com", "09876543211")) {
-            userController.setUserLog(userController.getUserHashMap().values().iterator().next());
-        } else {
-            fail("Login do usuário falhou.");
-        }
+        userController.setUserLog(userController.getUserHashMap().values().iterator().next());
 
         eventController.create("Test Event", "31/12/2024", "Description", "Location", "owner-id");
         sessionController.create("Test Event", "SessionId1", "01/12/2024", "Session Description", "Session Location", "08:00", "10:00", "owner-id", "Event");
@@ -65,14 +60,14 @@ public class AttendeeControllerTest {
 
         if (userController.loginValidate("newuser@example.com", "09876543211")) {
             userController.setUserLog(userController.getUserHashMap().values().iterator().next());
+        } else {
+            fail("Login do usuário falhou.");
         }
 
         eventController.create("Test Event", "31/12/2024", "Description", "Location", "owner-id");
         sessionController.create("Test Event", "SessionId1", "01/12/2024", "Session Description", "Session Location", "08:00", "10:00", "owner-id", "Event");
 
-
         attendeeController.create("Jake", "353738", userController.getData("id"));
-
         attendeeController.update("Jane", "353738");
 
         HashMap<String, Persistence> attendees = attendeeController.getAttendeeHashMap();
